15f2b32f55f72de643f86a135ad48e52da1e8d97
archit-garg12 <gargkarchit@gmail.com>
2020-02-23 01:26:17 -0800

    tokenizing

--- Posting.py
+++ Posting.py
@@ -1,8 +1,20 @@
 class Posting():
-    def __init__(self, doc_name:str, doc_id:int):
-        self.doc_name = doc_name
+    def __init__(self, doc_id:int):
         self.doc_id = doc_id
-        self.token_attrib = {}
-        self.tf_idf = 0
+        self.pos = []
+        self.tf = 0
+        self.idf = 0
+    def add_pos(self, positions: list) -> None:
+        self.pos = positions
+    def add_tf(self, term_freq:int) -> None:
+        self.tf = term_freq
+    def get_pos(self) -> list:
+        return self.pos
+    def get_tf(self) -> int:
+        return self.tf
+    def get_doc_id(self) -> int:
+        return self.doc_id
+    
+
 
     
