bd9c5fe86fe52956549246f3f0b44e46df523e65
Krishnateja Avvari <kavvari@uci.edu>
2020-02-22 23:06:02 -0800

    Returned root

--- Url.py
+++ Url.py
@@ -1,15 +1,20 @@
 from lxml import etree
 from io import *
+import json
 
 class Url:
     def __init__(self, json_file: str):
-        self._json_file = json_file
+        with open(json_file) as json_file:
+            data = json.load(json_file)
+            self._url = data['url']
+            self._content = data['content']
 
     def get_url(self) -> str:
-        return self._json_file['url']
+        return self._url
 
     def get_html(self) -> 'Etree ':
+        html_value = self._content
         parser = etree.HTMLParser()
         tree = etree.parse(StringIO(html_value), parser)
-        root = tree.getroot()
-        return self._json_file['content']
+        return tree.getroot()
+
