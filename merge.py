07d53ddae835a0d6930154978c499fc7d03ff58a
Krishnateja Avvari <kavvari@uci.edu>
2020-02-23 01:24:28 -0800

    merge files

--- merge.py
+++ merge.py
@@ -1,5 +1,23 @@
-def merge_two_files(file1: str, file2: str) -> None:
-    with open(file1) as f:
-        
+import os
+
+
+def merge_two_files(file1: str, file2: str, write_file: str) -> None:
+    with open(file1) as f1, open(file2) as f2, open(write_file, 'w+') as w:
+        for p1, p2 in zip(f1, f2):
+            line_split = p1.split()
+            word = line_split[0]
+            postings = eval(line_split[1])
+            postings2 = eval(p2.split()[1])
+            postings += postings2
+            write_file.write(word + ' ' + str(postings))
+    os.remove(file1)
+    os.remove(file2)
+
 
 def merge_total(directory: str) -> None:
+    listdir = os.listdir(directory)
+    if len(listdir) == 1:
+        return
+    merge_two_files(directory + 'file1.txt', directory + '/file2.txt', directory + 'final1.txt')
+    for i in range(2, len(listdir)):
+        merge_two_files(listdir[i], directory + 'final' + str(i-1) + '.txt', directory + 'final' + str(i) + '.txt')
