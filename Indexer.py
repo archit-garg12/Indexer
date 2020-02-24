c337bb620a9114473f8e71842fcb0dffe4c2d4e4
Dominic Ong <dominigo@uci.edu>
2020-02-23 20:16:00 -0800

    added index

--- Indexer.py
+++ Indexer.py
@@ -10,7 +10,7 @@ DOC_ID_DICT = dict()
 
 def get_all_files(dev_directory):
 
-    file_count_name = 'indexes/inverted_index_1'
+    file_count_name = 'indexes/inverted_index_'
     file_count = 0
     file_count_name_count = 1
     doc_id = 0
@@ -21,25 +21,29 @@ def get_all_files(dev_directory):
         if direct != '.DS_Store':
 
             for file in os.listdir(dev_directory + direct):
+                print(file_count)
                 try:
                     doc_id += 1
                     # posting(file) returns url and stuff for DOC_ID_DICT and for tokenizer
                     temp = Url(dev_directory + direct + '/' + file)
                     print(dev_directory + direct + '/' + file)
                     # write to a file the current inverted index, if it is above a certain file count
-                    if file_count >= 50:
-                        with open(file_count_name, 'w+') as count:
+                    file_count += 1
+                    reader.read_file(temp.get_html(), doc_id, inverted_index)
+                    if file_count == 1000:
+                        with open(file_count_name + str(file_count_name_count), 'w+') as count:
                             count.write('{\n')
                             s = sorted(inverted_index.items(), key=lambda x: x[0])
                             for k, v in s:
                                 count.write(str(k) + '#' + str([str(x) for x in v]) + '#\n')
                             count.write('}')
+                            count.write('\n\n\n\n')
+                        inverted_index = defaultdict(list)
+                    elif file_count > 1000:
                         file_count_name_count += 1
                         file_count = 0
-                        file_count_name = file_count_name[:-1] + str(file_count_name_count)
                         # adds the current dict to a file for a partial index
                         # change file_count_name also to write to a different file
-                    reader.read_file(temp.get_html(), doc_id, inverted_index)
                     DOC_ID_DICT[doc_id] = temp.get_url()
                 except Exception as e:
                     with open('error.txt', 'a+') as error_file:
@@ -53,7 +57,7 @@ def test():
     file_count_name = 'indexes/inverted_index_1'
 
     inv = defaultdict(list)
-    temp = Url('DEV/www_cs_uci_edu/f1a7ab7fb645fe5df395bac24cb4a09d8e8c239b5b0ec10783ec7381d8f7125e.json')
+    temp = Url('DEV/aiclub_ics_uci_edu/8ef6d99d9f9264fc84514cdd2e680d35843785310331e1db4bbd06dd2b8eda9b.json')
     print('penis lover 3000')
     reader = Html_Reader()
     print('fuck you krish')
